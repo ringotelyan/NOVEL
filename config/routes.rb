f843322e9bc9fffcbcebdef3c623e13c134463a2
ringotelyan <sute.dh9999@gmail.com>
2022-06-06 06:25:46 +0000

    [Update]devise関連ルーティング編集( 不要なルーティング削除)

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
 
   # 顧客用
+  # skipは不要なルーティングを削除
   # URL /users/sign_in ...
-  devise_for :users, controllers: {
+  devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
 
   # 管理者
+  # skipは不要なルーティングを削除
   # URL /admin/sign_in ...
-  devise_for :admin, controllers: {
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
 
