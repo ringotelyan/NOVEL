335e7d5b25135e87003b17ad6180c6e7baba07be
ringotelyan <sute.dh9999@gmail.com>
2022-06-11 13:22:46 +0000

    [Update]フォロー・フォロワー機能実装のみ

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,6 @@ Rails.application.routes.draw do
   # deviseに新たにルーティングを追加（スコープを経由）（ゲストログイン機能）
   devise_scope :user do
     post 'users/guest_sign_in', to: 'public/sessions#new_guest'
-    resource :relationships, only: [:create, :destroy]
-    get 'followings' => 'relationships#followings', as: 'followings'
-    get 'followers' => 'relationships#followers', as: 'followers'
   end
 
   # 管理者
@@ -30,7 +27,11 @@ Rails.application.routes.draw do
       resource :favorites, only: [:create, :destroy]
       resources :novel_comments, only: [:create, :destroy]
     end
-    resources :users, only: [:index, :show, :edit, :update]
+    resources :users, only: [:index, :show, :edit, :update] do
+      resource :relationships, only: [:create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
+    end
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
