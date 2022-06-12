2221ba078322320128dc8a55b42787f13d4ee2af
ringotelyan <sute.dh9999@gmail.com>
2022-06-12 04:05:49 +0000

    [Update]検索方法追加

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,2 +1,13 @@
 class Public::SearchesController < ApplicationController
+
+  def search
+    # params[:range]は検索モデル,params[:search]は検索方法,params[:word]は検索ワード
+    @range = params[:range]
+
+    if @range == "User"
+      @users = User.looks(params[:search], params[:word])
+    else
+      @novels = Novel.looks(params[:search], params[:word])
+    end
+  end
 end
