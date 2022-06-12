a8d9d228aaf9fec1b495d805ebe5c9ce68aafc6a
ringotelyan <sute.dh9999@gmail.com>
2022-06-12 04:29:43 +0000

    [Update]検索可能

--- app/models/novel.rb
+++ app/models/novel.rb
@@ -14,15 +14,15 @@ class Novel < ApplicationRecord
   # 検索方法分岐
   def self.looks(search, word)
     if search == "perfect_match"
-      @book = Book.where("title LIKE?", "#{word}")
+      @novel = Novel.where("title LIKE?", "#{word}")
     elsif search == "forward_match"
-      @book = Book.where("title LIKE?", "#{word}%")
+      @novel = Novel.where("title LIKE?", "#{word}%")
     elsif search == "backward_match"
-      @book =Book.where("title LIKE", "%#{word}")
+      @novel = Novel.where("title LIKE", "%#{word}")
     elsif search == "partial_match"
-      @book = Book.where("title LIKE?", "%#{word}%")
+      @novel = Novel.where("title LIKE?", "%#{word}%")
     else
-      @book = Book.all
+      @novel = Novel.all
     end
   end
 
