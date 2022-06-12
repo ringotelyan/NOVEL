b6f99c00039becc4b1ab1d82250edcfcbbe6484c
ringotelyan <sute.dh9999@gmail.com>
2022-06-12 15:25:24 +0000

    [Update]管理者ログイン準備

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
+
+  protected
+
+  def after_sign_in_path_for(resource)
+  end
+
+  def after_sign_out_path_for(resource)
+  end
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
