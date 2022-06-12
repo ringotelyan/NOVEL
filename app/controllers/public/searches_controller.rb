efc66552d2bfe1e864da77b6e6be39253b633760
ringotelyan <sute.dh9999@gmail.com>
2022-06-12 06:15:39 +0000

    [Update]小説本文内単語検索可能

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -3,6 +3,7 @@ class Public::SearchesController < ApplicationController
   def search
     # params[:range]は検索モデル,params[:search]は検索方法,params[:word]は検索ワード
     @range = params[:range]
+    @word = params[:word]
 
     if @range == "User"
       @users = User.looks(params[:search], params[:word])
