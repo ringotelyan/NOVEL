4afec164d18920e67c11584749ce737484d3a30e
ringotelyan <sute.dh9999@gmail.com>
2022-06-13 11:10:46 +0000

    [Update]いいね・コメント機能非同期通信

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -2,15 +2,15 @@ class Public::FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    novel = Novel.find(params[:novel_id])
-    favorite = current_user.favorites.new(novel_id: novel.id)
+    @novel = Novel.find(params[:novel_id])
+    favorite = current_user.favorites.new(novel_id: @novel.id)
     favorite.save
     # 画面遷移が行われ非同期処理が行われなくなるため、「redirect_to」の記述を削除
   end
 
   def destroy
-    novel = Novel.find(params[:novel_id])
-    favorite = current_user.favorites.find_by(novel_id: novel.id)
+    @novel = Novel.find(params[:novel_id])
+    favorite = current_user.favorites.find_by(novel_id: @novel.id)
     favorite.destroy
     # 画面遷移が行われ非同期処理が行われなくなるため、「redirect_to」の記述を削除
   end
