4ec5bde0098093156d70fb8b71be4fd852be070f
ringotelyan <sute.dh9999@gmail.com>
2022-06-13 06:32:57 +0000

    [Update]いいねコントローラ編集

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -4,14 +4,14 @@ class Public::FavoritesController < ApplicationController
     novel = Novel.find(params[:novel_id])
     favorite = current_user.favorites.new(novel_id: novel.id)
     favorite.save
-    redirect_to public_novel_path(novel)
+    # 画面遷移が行われ非同期処理が行われなくなるため、「redirect_to」の記述を削除
   end
 
   def destroy
     novel = Novel.find(params[:novel_id])
     favorite = current_user.favorites.find_by(novel_id: novel.id)
     favorite.destroy
-    redirect_to public_novel_path(novel)
+    # 画面遷移が行われ非同期処理が行われなくなるため、「redirect_to」の記述を削除
   end
 
 end
