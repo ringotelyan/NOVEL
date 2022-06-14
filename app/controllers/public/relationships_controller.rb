02710a22681cac7dbf94a7b4fecaeb1235e63191
ringotelyan <sute.dh9999@gmail.com>
2022-06-14 11:43:24 +0000

    [Update]フォロー・フォロワー非同期とコメント非同期

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -3,12 +3,16 @@ class Public::RelationshipsController < ApplicationController
   # フォローするとき
   def create
     current_user.follow(params[:user_id])
+    @users = User.where(is_deleted: false)
+    @user = User.find(params[:user_id])
     # "request.referer"は遷移前のURLを取得する際に使う
   end
 
   # フォロー外すとき
   def destroy
     current_user.unfollow(params[:user_id])
+    @users = User.where(is_deleted: false)
+    @user = User.find(params[:user_id])
   end
 
   # フォロー一覧
