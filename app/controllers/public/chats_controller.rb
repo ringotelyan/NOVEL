90d252a4339ce77b7958abf79e26441100afcb6f
ringotelyan <sute.dh9999@gmail.com>
2022-06-16 12:28:09 +0000

    [Update]チャット機能非同期

--- app/controllers/public/chats_controller.rb
+++ app/controllers/public/chats_controller.rb
@@ -3,12 +3,13 @@ class Public::ChatsController < ApplicationController
   def show
     # チャット相手のuser情報を取得
     @user = User.find(params[:id])
+    # binding.pry
     # user_roomsテーブルのuser_idが本人のレコードのroom_id配列で取得
     rooms = current_user.user_rooms.pluck(:room_id)
     # user_idがチャット相手で、room_idが本人の属するroom_id(配列)となるuser_roomsテーブルのレコードを取得して、user_room変数に格納
     # これによって、本人とチャット相手に共通のroom_idが存在していれば、その共通のroom_idとチャット相手のuser_idがuser_room変数に格納される（１レコード）。
     # 存在しなければnilになる。
-    user_rooms = UserRoom.find_by(user_id: @user_id, room_id: rooms)
+    user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
 
     # user_roomでルームを取得できなかった(本人(Aさん)とチャット相手(Bさん)のチャットがまだ存在しない)場合の処理
 
@@ -29,6 +30,7 @@ class Public::ChatsController < ApplicationController
 
     # roomに紐づくchatsテーブルのレコードを@chatsに格納
     @chats = @room.chats
+    #binding.pry
     # form_withでチャットを送信する際に必要な空のインスタンス
     # ここでroom.idを@chatに代入しておかないと、form_withで記述するroom_idに値が渡らない
     @chat = Chat.new(room_id: @room.id)
