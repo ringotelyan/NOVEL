b97453e96f5b5fb5afc029b08983360c3677e998
ringotelyan <sute.dh9999@gmail.com>
2022-06-17 02:10:00 +0000

    [Update]フォロー一覧退会者非表示

--- app/controllers/public/novels_controller.rb
+++ app/controllers/public/novels_controller.rb
@@ -25,7 +25,7 @@ class Public::NovelsController < ApplicationController
     # @novels = Novel.includes(:favorited_users)
     novels = Novel.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
     @novels = Kaminari.paginate_array(novels).page(params[:page]).per(10)
-   
+
       #sort {|a,b|
         #b.favorited_users.size <=>
         #a.favorited_users.size}
