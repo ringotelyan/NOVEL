a1ac1014ed20e5df23e9e7bbc89e0c3b226d10f4
ringotelyan <sute.dh9999@gmail.com>
2022-06-20 12:02:10 +0000

    [Update]投稿一覧表示ブックマーク順番

--- app/controllers/public/novels_controller.rb
+++ app/controllers/public/novels_controller.rb
@@ -23,9 +23,9 @@ class Public::NovelsController < ApplicationController
 
   def index
     # @novels = Novel.includes(:favorited_users)
-    novels = Novel.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
-    @novels = Kaminari.paginate_array(novels).page(params[:page]).per(10)
-
+    #novels = Novel.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
+    #@novels = Kaminari.paginate_array(novels).page(params[:page]).per(10)
+    @novels = Novel.left_joins(:active_favorites).group(:id).order('count(favorites.novel_id) desc').page(params[:page]).per(10)
       #sort {|a,b|
         #b.favorited_users.size <=>
         #a.favorited_users.size}
