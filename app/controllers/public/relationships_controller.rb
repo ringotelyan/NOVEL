1a2c729aad4a96aeec44b9832c96953803f82dff
ringotelyan <sute.dh9999@gmail.com>
2022-06-26 16:41:03 +0000

    [Update]フォローフォロワー一覧微調整

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -17,12 +17,12 @@ class Public::RelationshipsController < ApplicationController
   # フォロー一覧
   def followings
     @user = User.find(params[:user_id])
-    @users = @user.followings
+    @users = @user.followings.deleted
   end
 
   # フォロワー一覧
   def followers
     @user = User.find(params[:user_id])
-    @users = @user.followers
+    @users = @user.followers.deleted
   end
 end
