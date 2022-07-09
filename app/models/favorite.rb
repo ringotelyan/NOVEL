d93b423fb96b6c757c8f8c2d42068055fcf512be
ringotelyan <sute.dh9999@gmail.com>
2022-07-09 07:22:41 +0000

    [Update]1投稿1いいねバリデーション

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,8 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :novel
+
+  # １人１ゴフノベル１ブックマーク
+  # favoritesテーブルのuser_idカラムについて、一意性(uniquness)の制約をかける。ただし、novel_idの範囲内での一意性
+  validates :user_id, uniqueness: { scope: :novel_id }
 end
